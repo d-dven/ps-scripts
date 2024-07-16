cd6097401724167686783a4de03613efaa4cb0c4
username <MY_NAME@example.com>
2024-07-15 20:36:22 -0400

    initial files

---
+++ my_first_script.ps1
@@ -0,0 +1,42 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$FormObject = [System.Windows.Forms.Form]
+$LabelObject = [System.Windows.Forms.Label]
+$ButtonObject = [System.Windows.Forms.Button]
+
+$HelloWorldForm=New-Object $FormObject
+$HelloWorldForm.ClientSize='500,300'
+$HelloWorldForm.Text='Hello World'
+$HelloWorldForm.BackColor='#acedcf'
+
+$lbltitle=New-Object $LabelObject
+$lbltitle.Text='Hello World'
+$lbltitle.AutoSize=$true
+$lbltitle.ForeColor='#faedac'
+$lbltitle.Location=New-Object System.Drawing.Point(100,100)
+$lbltitle.Font='Georgia,30,style=Bold'
+
+$btnHello=New-Object $ButtonObject
+$btnHello.Text='Say Hello!'
+$btnHello.AutoSize=$true
+$btnHello.Font='Verdana,15'
+$btnHello.Location=New-Object System.Drawing.Point(200,225)
+
+$HelloWorldForm.Controls.AddRange(@($lbltitle, $btnHello))
+
+#Logic Section/Functions
+
+function SayHello{
+    if ($lbltitle.Text -eq '') {
+    $lbltitle.Text='Hello World!'
+    }else{
+        $lbltitle.Text='Haha hi'
+    }
+}
+##Add the functions to the form
+$btnHello.Add_Click({SayHello})
+
+#Display the form
+$HelloWorldForm.ShowDialog()
+
+#Cleans up the form
+$HelloWorldForm.Dispose()
---
+++ service_information_gui.ps1
@@ -0,0 +1,79 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$FormObject=[System.Windows.Forms.Form]
+$LabelObject=[System.Windows.Forms.Label]
+$ComboBoxObject=[System.Windows.Forms.ComboBox]
+
+$DefaultFont='Verdana,11'
+
+$AppForm=New-Object $FormObject
+
+$AppForm.ClientSize='500,200'
+$AppForm.Text='Service Inspector'
+$AppForm.BackColor='#faffaf'
+$AppForm.Font=$DefaultFont
+
+#Building the GUI
+$lblService=New-Object $LabelObject
+$lblService.Text='Services: '
+$lblService.AutoSize=$true
+$lblService.Location=New-Object System.Drawing.Point(20,20)
+
+$ddlService=New-Object $ComboBoxObject
+$ddlService.Width='300'
+$ddlService.Location=New-Object System.Drawing.Point(140,20)
+
+#Load the drop down list for services
+Get-Service | ForEach-Object {$ddlService.Items.Add($_.Name)}
+
+$ddlService.Text='Pick a Service'
+
+$lblForName=New-Object $LabelObject
+$lblForName.Text='Service Friendly Name: '
+$lblForName.AutoSize=$true
+$lblForName.Location=New-Object System.Drawing.Point(20,80)
+
+$lblName=New-Object $LabelObject
+$lblName.Text='test'
+$lblName.AutoSize=$true
+$lblName.Location=New-Object System.Drawing.Point(220,80)
+
+
+$lblForStatus=New-Object $LabelObject
+$lblForStatus.Text='Status: '
+$lblForStatus.AutoSize=$true
+$lblForStatus.Location=New-Object System.Drawing.Point(20,140)
+
+$lblStatus=New-Object $LabelObject
+$lblStatus.Text='test'
+$lblStatus.AutoSize=$true
+$lblStatus.Location=New-Object System.Drawing.Point(220, 140)
+
+
+$AppForm.Controls.AddRange(@($lblService, $ddlService, $lblForName, $lblName, $lblStatus, $lblForStatus))
+
+#Add some functions to form
+function GetServiceDetails{
+    $ServiceName=$ddlService.SelectedItem
+    Write-Host "Service Name : $ServiceName"
+ ## This line is for testing if the retrieval of ServiceName upon clicking the button
+
+    $details=Get-Service -Name $ServiceName | select displayname,status
+    $lblName.Text=$details.displayname
+    $lblStatus.Text=$details.status
+
+    #logic for color change
+    if ($lblStatus.Text -eq 'Running') {
+        $lblStatus.ForeColor='green'
+    }else{
+        $lblStatus.ForeColor='red'
+    }
+}
+
+##Add functions to the controls
+$ddlService.Add_SelectedIndexChanged({GetServiceDetails})
+
+#Show Form
+$AppForm.ShowDialog()
+
+#Garbage Collection
+$AppForm.Dispose()
